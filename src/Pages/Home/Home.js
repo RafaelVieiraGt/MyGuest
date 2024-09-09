2f42383139105035aede8f9e2fc34455b0b3d8be
Rafael Camilli Vieira <rafaelcamillivieira@gmail.com>
2024-09-09 20:24:36 -0300

    Add box-home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,18 @@
+import CardHome from "../../Components/CardHome/CardHome";
+import './home.css'
 import Header from "../../Components/Header/Header";
 
 export default function Home() {
     return (
-        <Header />
+        <>
+            <Header />
+            <body>
+                <div className="card-rows" >
+                    <CardHome title="Entradas" />
+                    <CardHome title="Saidas" />
+                    <CardHome title="Saldo" />
+                </div>
+            </body>
+        </>
     )
 }
